a284d2ea4c8ce094ede6192a92f3e7ca7ad8c8c8
Damir Minigaliev <dminigaliev@gmail.com>
2025-01-25 15:16:25 +0500

    Поправил код в соответствии с найденными ошибками в тестах постмана, чекстайл

--- src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
-import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.Genre.GenreDbStorage;
 
