5636d457eff57500ec28655cfdf3062fb43c7e95
Ruslan Tsyplakov <stoiksevera1@yandex.ru>
2025-02-01 19:45:29 +0300

    реализация фильтра пор режисерам и сортировки по годам и лайкам 3

--- src/main/java/ru/yandex/practicum/filmorate/storage/mappers/FilmRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mappers/FilmRowMapper.java
@@ -11,7 +11,6 @@ import ru.yandex.practicum.filmorate.storage.Mpa.MpaDbStorage;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
